262c7528f096caecae1ce67732b8838e504040d2
kalonzo <kalonzo@bluewin.ch>
2019-03-14 20:22:26 +0100

    correctif

--- src/Controller/HomepageController.php
+++ src/Controller/HomepageController.php
@@ -16,4 +16,25 @@ class HomepageController extends AbstractController
             'controller_name' => 'HomepageController',
         ]);
     }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(\Swift_Mailer $mailer)
+    {
+        return $this->render('homepage/contact.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function about(\Swift_Mailer $mailer)
+    {
+        return $this->render('homepage/about.html.twig', [
+            'controller_name' => 'HomepageController',
+        ]);
+    }
+
 }
