6a8492ec4781004f612e98efa7b24d282b82a965
kalonzo <kalonzo@bluewin.ch>
2019-08-26 06:20:59 +0200

    Index portfolio

--- portfolio_0.php
+++ portfolio_0.php
@@ -1,126 +1,153 @@
 <!-- 1 start -->
 <div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_4.png" alt="">
+	<img class="portfolio_image" src="images/portfolio/projects_pic/pic_1.png" alt="">
 	<div class="portfolio_info">
 		<div class="portfolio_center">
-			<a class="pp pp_image" href="images/portfolio/pic_4.png" title=""></a>
-			<a class="link" href="" title=""></a>
+			<a class="pp pp_image" href="images/portfolio/projects_pic/pic_22.png" title=""></a>
+			<a class="link" href="https://favelas-store.com" title="FAVELAS"></a>
 			<div class="clear"></div>
 			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
+			<h4>Favelas</h4>
+			<p>
+				                                                        WP Framework                                                        
+			</p>
 		</div>
 	</div>
 </div>
 <!-- 1 ends -->
 <!-- 1 start -->
 <div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_5.png" alt="">
+	<img class="portfolio_image" src="images/portfolio/projects_pic/pic_2.png" alt="">
 	<div class="portfolio_info">
 		<div class="portfolio_center">
-			<a class="pp pp_image" href="images/portfolio/pic_5.png" title=""></a>
-			<a class="link" href="" title=""></a>
+			<a class="pp pp_video" href="https://www.youtube.com/embed/UnA7zh2n6BE" title="TREND ON LINE"></a>
+			<a class="link" href="https://trend-on-line.com" title="TREND"></a>
 			<div class="clear"></div>
 			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
+			<h4>Trend On Line</h4>
+			<p>
+				                                                            WP Framework                                                        
+			</p>
 		</div>
 	</div>
 </div>
 <!-- 1 ends -->
-
 <!-- 1 start -->
 <div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_6.png" alt="">
+	<img class="portfolio_image" src="images/portfolio/projects_pic/pic_3.png" alt="">
 	<div class="portfolio_info">
 		<div class="portfolio_center">
-			<a class="pp pp_image" href="images/portfolio/pic_6.png" title=""></a>
+			<a class="pp pp_image" href="images/portfolio/projects_pic/pic_6.png" title=""></a>
 			<a class="link" href="" title=""></a>
 			<div class="clear"></div>
 			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
+			<h4>Bientôt em Ligne</h4>
+			<p>
+				                                                            WP Framework                                                        
+			</p>
 		</div>
 	</div>
 </div>
 <!-- 1 ends -->
-
-<!-- 1 start -->
+<!-- 2 start -->
 <div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_7.png" alt="">
+	<img class="portfolio_image" src="images/portfolio/projects_pic/pic_7.png" alt="">
 	<div class="portfolio_info">
 		<div class="portfolio_center">
-			<a class="pp pp_image" href="images/portfolio/pic_7.png" title=""></a>
+			<a class="pp pp_image" href="images/portfolio/projects_pic/pic_8.png" title=""></a>
 			<a class="link" href="" title=""></a>
 			<div class="clear"></div>
 			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
+			<h4>DS - Bientôt en version demo sur mon serveur test</h4>
+			<p>
+				                                                            Framework                                                        
+			</p>
 		</div>
 	</div>
 </div>
-<!-- 1 ends -->
-
-<!-- 1 start -->
+<!-- 2 ends -->
+<!-- 2 start -->
 <div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_8.png" alt="">
+	<img class="portfolio_image" src="images/portfolio/projects_pic/pic_9.png" alt="">
 	<div class="portfolio_info">
 		<div class="portfolio_center">
-			<a class="pp pp_image" href="images/portfolio/pic_8.png" title=""></a>
+			<a class="pp pp_image" href="images/portfolio/projects_pic/pic_10.png" title=""></a>
 			<a class="link" href="" title=""></a>
 			<div class="clear"></div>
 			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
+			<h4>HX Agency - Bientôt em Ligne</h4>
+			<p>
+				                                                            Framework                                                        
+			</p>
 		</div>
 	</div>
 </div>
-<!-- 1 ends -->
-
-<!-- 1 start -->
+<!-- 2 ends -->
+<!-- 3 start -->
 <div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_1.png" alt="">
+	<img class="portfolio_image" src="images/portfolio/projects_pic/pic_11.png" alt="">
 	<div class="portfolio_info">
 		<div class="portfolio_center">
-			<a class="pp pp_image" href="images/portfolio/pic_1.png" title=""></a>
+			<a class="pp pp_image" href="images/portfolio/projects_pic/pic_12.png" title=""></a>
 			<a class="link" href="" title=""></a>
 			<div class="clear"></div>
 			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
+			<h4>Carte de visite perso</h4>
+			<p>
+				                                                            Design                                                        
+			</p>
 		</div>
 	</div>
 </div>
-<!-- 1 ends -->
-
-<!-- 1 start -->
+<!-- 3 ends -->
+<!-- 3 start -->
 <div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_2.png" alt="">
+	<img class="portfolio_image" src="images/portfolio/projects_pic/pic_13.png" alt="">
 	<div class="portfolio_info">
 		<div class="portfolio_center">
-			<a class="pp pp_video" href="https://www.youtube.com/embed/617ANIA5Rqs?rel=0&amp;wmode=transparent" title="The Knife: We Share Our Mother's Health"></a>
+			<a class="pp pp_image" href="images/portfolio/projects_pic/pic_14.png" title=""></a>
 			<a class="link" href="" title=""></a>
 			<div class="clear"></div>
 			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
+			<h4>Logo Morgan</h4>
+			<p>
+				                                                            Design                                                        
+			</p>
 		</div>
 	</div>
 </div>
-<!-- 1 ends -->
-
-<!-- 1 start -->
+<!-- 3 ends -->
+<!-- 4 start -->
 <div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_3.png"  alt="">
+	<img class="portfolio_image" src="images/portfolio/projects_pic/pic_16.png" alt="">
 	<div class="portfolio_info">
 		<div class="portfolio_center">
-			<a class="pp pp_video" href="https://player.vimeo.com/video/2285902" title="R&ouml;yksopp: Remind Me"></a>
+			<a class="pp pp_image" href="images/portfolio/projects_pic/pic_17.png" title=""></a>
 			<a class="link" href="" title=""></a>
 			<div class="clear"></div>
 			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
+			<h4>News Letter appel d'offre</h4>
+			<p>
+				                                                            Newsletter                                                        
+			</p>
 		</div>
 	</div>
 </div>
-<!-- 1 ends -->
+<!-- 4 ends -->
+<!-- 4 start -->
+<div class="portfolio_item">
+	<img class="portfolio_image" src="images/portfolio/projects_pic/pic_15.png" alt="">
+	<div class="portfolio_info">
+		<div class="portfolio_center">
+			<a class="pp pp_video" href="https://www.youtube.com/embed/N2IQLAs68t0" title=""></a>
+			<a class="link" href="" title=""></a>
+			<div class="clear"></div>
+			<div class="mb_portfolio"></div>
+			<h4>Menu - Flyer</h4>
+			<p>
+				                                                            Newsletter                                                        
+			</p>
+		</div>
+	</div>
+</div>
+<!-- 4 ends -->
--- portfolio_1.php
+++ portfolio_1.php
@@ -1,127 +1,50 @@
-
 <!-- 1 start -->
 <div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_8.png" alt="">
+	<img class="portfolio_image" src="images/portfolio/projects_pic/pic_1.png" alt="">
 	<div class="portfolio_info">
 		<div class="portfolio_center">
-			<a class="pp pp_image" href="images/portfolio/pic_8.png" title=""></a>
+			<a class="pp pp_image" href="images/portfolio/projects_pic/pic_22.png" title="">Favelas</a>
 			<a class="link" href="" title=""></a>
 			<div class="clear"></div>
 			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
-		</div>
-	</div>
-</div>
-<!-- 1 ends -->
-
-<!-- 1 start -->
-<div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_4.png" alt="">
-	<div class="portfolio_info">
-		<div class="portfolio_center">
-			<a class="pp pp_image" href="images/portfolio/pic_4.png" title=""></a>
-			<a class="link" href="" title=""></a>
-			<div class="clear"></div>
-			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
-		</div>
-	</div>
-</div>
-<!-- 1 ends -->
-
-<!-- 1 start -->
-<div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_6.png" alt="">
-	<div class="portfolio_info">
-		<div class="portfolio_center">
-			<a class="pp pp_image" href="images/portfolio/pic_6.png" title=""></a>
-			<a class="link" href="" title=""></a>
-			<div class="clear"></div>
-			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
-		</div>
-	</div>
-</div>
-<!-- 1 ends -->
-
-<!-- 1 start -->
-<div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_7.png" alt="">
-	<div class="portfolio_info">
-		<div class="portfolio_center">
-			<a class="pp pp_image" href="images/portfolio/pic_7.png" title=""></a>
-			<a class="link" href="" title=""></a>
-			<div class="clear"></div>
-			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
+			<h4>Favelas</h4>
+			<p>
+				                                                        WP Framework                                                        
+			</p>
 		</div>
 	</div>
 </div>
 <!-- 1 ends -->
 <!-- 1 start -->
 <div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_1.png" alt="">
+	<img class="portfolio_image" src="images/portfolio/projects_pic/pic_2.png" alt="">
 	<div class="portfolio_info">
 		<div class="portfolio_center">
-			<a class="pp pp_image" href="images/portfolio/pic_1.png" title=""></a>
+			<a class="pp pp_video" href="https://www.youtube.com/embed/UnA7zh2n6BE" title="TREND ON LINE"></a>
 			<a class="link" href="" title=""></a>
 			<div class="clear"></div>
 			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
+			<h4>Trend On Line</h4>
+			<p>
+				                                                            WP Framework                                                        
+			</p>
 		</div>
 	</div>
 </div>
 <!-- 1 ends -->
-
 <!-- 1 start -->
 <div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_2.png" alt="">
+	<img class="portfolio_image" src="images/portfolio/projects_pic/pic_3.png" alt="">
 	<div class="portfolio_info">
 		<div class="portfolio_center">
-			<a class="pp pp_video" href="https://www.youtube.com/embed/617ANIA5Rqs?rel=0&amp;wmode=transparent" title="The Knife: We Share Our Mother's Health"></a>
+			<a class="pp pp_image" href="images/portfolio/projects_pic/pic_6.png" title=""></a>
 			<a class="link" href="" title=""></a>
 			<div class="clear"></div>
 			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
-		</div>
-	</div>
-</div>
-<!-- 1 ends -->
-
-<!-- 1 start -->
-<div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_3.png"  alt="">
-	<div class="portfolio_info">
-		<div class="portfolio_center">
-			<a class="pp pp_video" href="https://player.vimeo.com/video/2285902" title="R&ouml;yksopp: Remind Me"></a>
-			<a class="link" href="" title=""></a>
-			<div class="clear"></div>
-			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
-		</div>
-	</div>
-</div>
-<!-- 1 ends -->
-
-
-<!-- 1 start -->
-<div class="portfolio_item">
-	<img class="portfolio_image" src="images/portfolio/pic_5.png" alt="">
-	<div class="portfolio_info">
-		<div class="portfolio_center">
-			<a class="pp pp_image" href="images/portfolio/pic_5.png" title=""></a>
-			<a class="link" href="" title=""></a>
-			<div class="clear"></div>
-			<div class="mb_portfolio"></div>
-			<h4>Grand Design For Beginner</h4>
-			<p>WP Framework</p>
+			<h4>Bientôt em Ligne</h4>
+			<p>
+				                                                            WP Framework                                                        
+			</p>
 		</div>
 	</div>
 </div>
